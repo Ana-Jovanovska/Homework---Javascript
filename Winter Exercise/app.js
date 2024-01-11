248de180257ca9139df6f58ff5ca2f35df8ce58a
Ana-Jovanovska <150249028+Ana-Jovanovska@users.noreply.github.com>
2024-01-11 15:46:04 +0100

    Winter Exercise (Calculator)

---
+++ Winter Exercise/app.js
@@ -0,0 +1,12 @@
+console.log("Calculate is working");
+
+const numbersEl = document.querySelector("#number");
+const operatorEl = document.querySelector("#operator-one");
+const operatorTwoEl = document.querySelector("#operator-two");
+const delteEl = document.querySelector("#delete");
+const divisionEl = document.querySelector("#division");
+const multiplicationEl = document.querySelector("#multiplication");
+const minusEl = document.querySelector("minus");
+const plusEl = document.querySelector("plus");
+const zeroEl = document.querySelector("zero");
+const equalEl = document.querySelector("equal");
